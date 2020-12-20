618b1347e376f295ac57a5b472717aafd7987aa8
DharwinJRuppa <dharwinruppa1422@gmail.com>
2020-12-21 01:55:14 +0530

    oauth

---
+++ README.md
@@ -0,0 +1,17 @@
+# oauth-app
+## Authorization and Authentication by Auth0 Service using NodeJs
+## Live Demo: https://dharwin-oauth-app.herokuapp.com
+
+<img src="demo_images/0.png">
+
+<img src="demo_images/1.png">
+
+<img src="demo_images/2.png">
+
+<img src="demo_images/3.png">
+
+<img src="demo_images/4.png">
+
+<img src="demo_images/5.png">
+
+<img src="demo_images/6.png">
---
+++ server.js
@@ -0,0 +1,43 @@
+//imports
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+const { auth, requiresAuth } = require("express-openid-connect");
+require("dotenv/config");
+//middleware
+app.use(cors());
+app.use(morgan("dev"));
+//body-parser
+app.use(express.json());
+//view-engine
+app.set("view engine", "ejs");
+//autherization&authetication
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: "https://dharwinruppa1422.us.auth0.com",
+    baseURL: "http://dharwin-oauth-app.herokuapp.com",
+    clientID: "3M0jctyQxfIlLUDeEM8sK7EHELNMRwLn",
+    secret: "zabcdefghijklmnopqrstuvwxyzabcdefghfsdfkdsfjoi",
+  })
+);
+//router-config
+
+//router
+app.get("/", (req, res) => {
+  req.oidc.isAuthenticated()
+    ? res.redirect("/profile")
+    : res.redirect("/login");
+});
+app.get("/profile", requiresAuth(), (req, res) => {
+  console.log(req.oidc.user);
+  res.render("index", { info: req.oidc.user });
+});
+//server
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log("application is started " + port);
+});
